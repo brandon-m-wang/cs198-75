05fb755566466ad93eb6cc38852210f924fa6867
brandon-m-wang <brandonwang0503@berkeley.edu>
2021-12-20 01:55:45 -0800

    html entity encoding for html lang support

--- .eleventy.js
+++ .eleventy.js
@@ -2,10 +2,16 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/css");
   eleventyConfig.addPassthroughCopy("./src/assets");
   eleventyConfig.addPassthroughCopy("./src/js");
-  eleventyConfig.addPassthroughCopy('./src/admin');
+  eleventyConfig.addPassthroughCopy("./src/admin");
 
-  eleventyConfig.addShortcode("handleCodeBlock", function(cb) { 
-    return `<pre><code class=language-${cb.lang}>${cb.code}</code></pre>`
+  function entityEncoder(html) {
+    return html.replace(/[\u00A0-\u9999<>\&]/g, function (i) {
+      return "&#" + i.charCodeAt(0) + ";";
+    });
+  }
+
+  eleventyConfig.addShortcode("handleCodeBlock", function (cb) {
+    return `<pre><code class=language-${cb.lang}>${entityEncoder(cb.code)}</code></pre>`;
   });
 
   return {
